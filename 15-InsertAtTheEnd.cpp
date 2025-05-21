aa523fcf742fd2f4a13a1d45054ead51cea38dcf
bilmoh287 <142092794+bilmoh287@users.noreply.github.com>
2025-05-22 01:23:49 +0300

    Add 15-InsertAtTheEnd

---
+++ 15-InsertAtTheEnd.cpp
@@ -0,0 +1,115 @@
+#include <iostream>
+using namespace std;
+
+// Creating a node
+class Node
+
+{
+public:
+    int value;
+    Node* next;
+    Node* prev;
+};
+
+void InsertAtBeginning(Node*& head, int Value)
+{
+    Node* new_node = new Node();
+    new_node->next = head;
+    new_node->value = Value;
+    new_node->prev = NULL;
+
+    if (head != NULL)
+    {
+        head->prev = new_node;
+    }
+
+    head = new_node;
+}
+
+void PrintNodeDetails(Node* head)
+{
+
+    if (head->prev != NULL)
+        cout << head->prev->value;
+    else
+        cout << "NULL";
+
+    cout << " <--> " << head->value << " <--> ";
+
+    if (head->next != NULL)
+        cout << head->next->value << "\n";
+    else
+        cout << "NULL";
+
+}
+
+// Print the linked list
+void PrintListDetails(Node* head)
+
+{
+    cout << "\n\n";
+    while (head != NULL) {
+        PrintNodeDetails(head);
+        head = head->next;
+    }
+}
+
+void InsertAtTheEnd(Node*& head, int Value)
+{
+    Node* new_nod = new Node();
+    new_nod->next = NULL;
+    new_nod->value = Value;
+
+    if (head == NULL)
+    {
+        new_nod->prev = NULL;
+        head = new_nod;
+    }
+    else 
+    {
+        Node* Current = head;
+        while (Current->next != NULL)
+        {
+            Current = Current->next;
+        }
+        Current->next = new_nod;
+        new_nod->prev = Current;
+    }
+}
+
+// Print the linked list
+void PrintList(Node* head)
+
+{
+    cout << "NULL <--> ";
+    while (head != NULL) {
+        cout << head->value << " <--> ";
+        head = head->next;
+    }
+    cout << "NULL";
+
+}
+
+int main()
+{
+    Node* head = NULL;
+
+    InsertAtBeginning(head, 5);
+    InsertAtBeginning(head, 4);
+    InsertAtBeginning(head, 3);
+    InsertAtBeginning(head, 2);
+    InsertAtBeginning(head, 1);
+
+    cout << "\nLinked List Contenet:\n";
+    PrintList(head);
+    PrintListDetails(head);
+
+    InsertAtTheEnd(head, 500);
+    //InsertAtBeginning(head, 0);
+
+    cout << "\n\n\nLinked List Contenet after Insertion:\n";
+    PrintList(head);
+    PrintListDetails(head);
+
+}
+
